4f2a061184a4589d6ea027f48e0e1dac41bb4ce5
Phạm Tuân <52484769+phamtuan090720@users.noreply.github.com>
2022-04-16 21:22:01 +0700

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# How to use CyberGoogleAuthentication ?
+# How to use CyberGoogleAuthentication?
+
+ Step 1 :  open file config.env  and fill in the information 
+
+> take your client_id and client_secret in here [Google Developers Console](https://console.cloud.google.com/apis)
+
+    PORT = 3000
+    MONGO_CONNECT_URL=<mongo_connect_url>
+    GOOGLE_CLIENT_ID =<your_google_client_id>
+    GOOGLE_CLIENT_SECRET = <your_google_client_secret>
+Step 2 :  install packet :
+ `npm install`
+Step 3 :  run server
+ `npm run dev`
+Step 4 : Open the link [http://localhost:3000/](http://localhost:3000/)
+Happy Hacking Goodluck !!
+ 
--- config/passport.js
+++ config/passport.js
@@ -1,50 +1,51 @@
-const GoogleStrategy = require('passport-google-oauth20').Strategy
-const mongoose = require('mongoose')
-const User = require('../models/User')
-
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const User = require("../models/User");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config/config.env" });
 module.exports = function (passport) {
   passport.use(
     new GoogleStrategy(
       {
-        clientID: '26388108577-11r1hpssfvnvl3mk9rr5ivmsupoaic82.apps.googleusercontent.com',
-        clientSecret: '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',
-        callbackURL: '/auth/google/callback',
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: "/auth/google/callback",
       },
       async (accessToken, refreshToken, profile, done) => {
-        //get the user data from google 
+        //get the user data from google
         const newUser = {
           googleId: profile.id,
           displayName: profile.displayName,
           firstName: profile.name.givenName,
           lastName: profile.name.familyName,
           image: profile.photos[0].value,
-          email: profile.emails[0].value
-        }
+          email: profile.emails[0].value,
+        };
 
         try {
-          //find the user in our database 
-          let user = await User.findOne({ googleId: profile.id })
+          //find the user in our database
+          let user = await User.findOne({ googleId: profile.id });
 
           if (user) {
             //If user present in our database.
-            done(null, user)
+            done(null, user);
           } else {
             // if user is not preset in our database save user data to database.
-            user = await User.create(newUser)
-            done(null, user)
+            user = await User.create(newUser);
+            done(null, user);
           }
         } catch (err) {
-          console.error(err)
+          console.error(err);
         }
       }
     )
-  )
+  );
 
   passport.serializeUser((user, done) => {
-    done(null, user.id)
-  })
+    done(null, user.id);
+  });
 
   passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => done(err, user))
-  })
-} 
+    User.findById(id, (err, user) => done(err, user));
+  });
+};
